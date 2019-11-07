de325a4a3d2391f14a3e958d4d7a49f7706514b4
Gabriel Hideki Higa <32941241+GabrielHiga@users.noreply.github.com>
2019-11-07 16:11:32 -0300

    Retirando valor "Hora" de exemple

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function carregar(){
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 13
+    var hora = data.getHours()
+
     msg.innerHTML = `Agora são ${hora} Horas`
 
     if(hora >= 0 && hora < 12){
